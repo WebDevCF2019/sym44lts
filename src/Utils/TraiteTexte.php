a437526b16d4b2acbe7555763ff778fa2c664bbb
mikhawa <michaeljpitz@gmail.com>
2019-12-06 11:00:07 +0100

    src/Utils/TraiteTexte.php

--- src/Utils/TraiteTexte.php
+++ src/Utils/TraiteTexte.php
@@ -11,6 +11,19 @@ class TraiteTexte
      * méthode statique permettant grâce à ces arguments de couper un texte sans couper les mots
      * Raccourci(string $texte, int $length): string
      */
+    public static function Raccourci(string $texte,int $length): string {
 
+        // texte plus cours que la longueur de découpe, on renvoie le texte non modifié
+        if(strlen($texte)<=$length) return $texte;
+
+        // on coupe à la longueur indiquée, sans tenir compte de la césure des mots
+        $coupetexte = substr($texte,0,$length);
+
+        // on recoupe la chaîne au dernier espace trouvé
+        $coupefinal = substr($coupetexte,0,strrpos($coupetexte," ") );
+
+        return $coupefinal;
+
+    }
 
 }
