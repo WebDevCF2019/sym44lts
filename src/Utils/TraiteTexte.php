7c3a2f30801d4c887246ae92fe7c462b4170b06c
mikhawa <michaeljpitz@gmail.com>
2019-12-06 10:42:14 +0100

    exe creation d'un service

---
+++ src/Utils/TraiteTexte.php
@@ -0,0 +1,16 @@
+<?php
+
+
+namespace App\Utils;
+
+
+class TraiteTexte
+{
+
+    /**
+     * méthode statique permettant grâce à ces arguments de couper un texte sans couper les mots
+     * Raccourci(string $texte, int $length): string
+     */
+
+
+}
