519de6688a815b345bc02e5fb44e71c2e7a78925
mikhawa <michaeljpitz@gmail.com>
2019-12-15 20:59:56 +0100

    change AdminUserFixtures.php

--- src/DataFixtures/AdminUserFixtures.php
+++ src/DataFixtures/AdminUserFixtures.php
@@ -2,15 +2,41 @@
 
 namespace App\DataFixtures;
 
+use App\Entity\AdminUser;
 use Doctrine\Bundle\FixturesBundle\Fixture;
 use Doctrine\Common\Persistence\ObjectManager;
+use Symfony\Component\Security\Core\Encoder\UserPasswordEncoderInterface;
 
 class AdminUserFixtures extends Fixture
 {
+    private $passwordEncoder;
+
+    public function __construct(UserPasswordEncoderInterface $passwordEncoder){
+        $this->passwordEncoder = $passwordEncoder;
+    }
     public function load(ObjectManager $manager)
     {
-        // $product = new Product();
-        // $manager->persist($product);
+        $adminUser = new AdminUser();
+        $login = "Admin";
+        $mdp = "Admin";
+
+        $adminUser
+            ->setUsername($login)
+            ->setRoles(['ROLE_USER','ROLE_ADMIN'])
+            ->setPassword($this->passwordEncoder->encodePassword($adminUser,$mdp));
+
+        $manager->persist($adminUser);
+
+        $adminUser = new AdminUser();
+        $login = "Lulu";
+        $mdp = "Lulu";
+
+        $adminUser
+            ->setUsername($login)
+            ->setRoles(['ROLE_USER'])
+            ->setPassword($this->passwordEncoder->encodePassword($adminUser,$mdp));
+
+        $manager->persist($adminUser);
 
         $manager->flush();
     }
