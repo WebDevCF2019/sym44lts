b7a345ccb26bc6096d2df0690740e24bb1f4ca30
mikhawa <michaeljpitz@gmail.com>
2019-12-17 16:21:22 +0100

    slug amélioré

--- src/Utils/Slug.php
+++ src/Utils/Slug.php
@@ -8,6 +8,28 @@ class Slug
 {
     public static function slugletexte(string $string): string
     {
-        return preg_replace('/\s+/', '-', mb_strtolower(trim(strip_tags($string)), 'UTF-8'));
+        // replace non letter or digits by -
+        $text = preg_replace('~[^\pL\d]+~u', '-', $string);
+
+        // transliterate
+        $text = iconv('utf-8', 'us-ascii//TRANSLIT', $text);
+
+        // remove unwanted characters
+        $text = preg_replace('~[^-\w]+~', '', $text);
+
+        // trim
+        $text = trim($text, '-');
+
+        // remove duplicate -
+        $text = preg_replace('~-+~', '-', $text);
+
+        // lowercase
+        $text = strtolower($text);
+
+        if (empty($text)) {
+            return 'n-a';
+        }
+
+        return $text;
     }
 }
